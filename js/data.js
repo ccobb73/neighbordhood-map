6a71ca085fed4993bc9e372675380fc8da9e5931
ccobb73 <che_cobb@yahoo.com>
2018-07-02 09:10:45 -0400

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,46 @@
+/* Model Data */
+
+var locations = [
+    {
+        title: 'Florida Surf Museum',
+        location: {
+            lat: 28.3571705,
+            lng: -80.60775660000002
+        }
+    },
+    {
+        title: 'Cocoa Beach Skatepark',
+        location: {
+            lat: 28.3183865,
+            lng: -80.62524910000002
+        }
+    },
+    {
+        title: 'Fin Expeditions Kayak Tours',
+        location: {
+            lat: 28.3095754,
+            lng: -80.61413379999999
+        }
+    },
+    {
+        title: 'Cocoa Beach Pier',
+        location: {
+            lat: 28.3677259,
+            lng: -80.60278370000003
+        }
+    },
+    {
+        title: 'Dinosaur Store',
+        location: {
+            lat: 28.3573746,
+            lng: -80.6100553
+        }
+    },
+    {
+        title: 'Escape Cocoa Beach',
+        location: {
+            lat: 28.3695937,
+            lng: -80.60753969999996
+        }
+    }
+];
